8d75d6760ddab9a91dc278da463c4bddacbb4f88
dipesharora <dipesharora@msn.com>
2022-09-25 21:24:46 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,11 @@ output "vm_id_output" {
 output "vm_name" {
   value = "${azurerm_linux_virtual_machine.ubuntuvm.*.name}"
 }
+
+output "vm_private_ip_address" {
+  value = "${azurerm_linux_virtual_machine.ubuntuvm.*.private_ip_address}"
+}
+
+output "vm_public_ip_address" {
+  value = "${azurerm_linux_virtual_machine.ubuntuvm.*.public_ip_address}"
+}
